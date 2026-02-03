40719bd7d2c4d8f87911335ec0bd9872024f1f30
Tommy Xaypanya <Tommy@gain.energy>
2026-02-03 01:02:04 -0600

    Disable eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
