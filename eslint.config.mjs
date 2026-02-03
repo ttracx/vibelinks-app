4713434e2788bee1087e21748869e2209ed743ce
Tommy Xaypanya <Tommy@gain.energy>
2026-02-03 01:01:23 -0600

    Fix Stripe API version and simplify ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-    },
-  },
+  ...compat.extends("next/core-web-vitals"),
 ];
 
 export default eslintConfig;
